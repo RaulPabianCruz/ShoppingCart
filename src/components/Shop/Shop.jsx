bb2e169ea9ceba28b3179e7d8e77c86f3093dddd
RaulPabianCruz <ito.pabian@gmail.com>
2024-06-12 17:36:42 -0700

    Create Shop component for the Shop page

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,11 +1,21 @@
-import { useOutletContext } from 'react-router-dom';
-import { useEffect } from 'react';
+import useShopItems from './hooks/useShopItems';
 import Card from '../Card/Card';
 
 function Shop() {
+  const { shopItems, error, loading } = useShopItems();
+
+  if (error) return <p>There was an error U_U</p>;
+
+  if (loading) return <p>Loading up the selection!</p>;
+
   return (
     <div>
-      <h1>Placeholder Shop</h1>
+      <h1>The e-Mart's Top Selection</h1>
+      <div>
+        {shopItems.map((item) => {
+          return <Card product={item} key={item.id} />;
+        })}
+      </div>
     </div>
   );
 }
