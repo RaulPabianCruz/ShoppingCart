6d391bc071ab19d4ef404a7e1c27da81886d16e2
RaulPabianCruz <ito.pabian@gmail.com>
2024-06-24 20:43:46 -0700

    Add the Cart component of the project

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,37 @@
+import { useOutletContext } from 'react-router-dom';
+import Item from '../Item/Item';
+import styles from './Cart.module.css';
+
 function Cart() {
+  const [cartItems] = useOutletContext();
+
+  const priceTotal = cartItems.reduce((accumulator, item) => {
+    const price = Number(item.quantity) * Number(item.product.price);
+    return accumulator + price;
+  }, 0);
+
+  const numOfItems = cartItems.reduce(
+    (accumulator, item) => accumulator + Number(item.quantity),
+    0,
+  );
+
   return (
-    <div>
-      <h1>PlaceHolder Cart</h1>
+    <div className={styles.cartContainer}>
+      <h1 className={styles.cartHeading}>Your Cart: </h1>
+      <div className={styles.infoHeaderContainer}>
+        <h2 className={styles.infoHeader}>Item</h2>
+        <h2 className={styles.infoHeader}>Quantity</h2>
+        <h2 className={styles.infoHeader}>Price</h2>
+      </div>
+      <div className={styles.itemListContainer}>
+        {cartItems.map((cartItem) => {
+          return <Item item={cartItem} key={cartItem.product.id} />;
+        })}
+      </div>
+      <h1 data-testid="total-display" className={styles.totalDisplay}>
+        Total({numOfItems} Item{numOfItems != 1 ? 's' : ''}): $
+        {priceTotal.toFixed(2)}
+      </h1>
     </div>
   );
 }
