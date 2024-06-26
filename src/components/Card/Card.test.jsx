3ce3f6c7e208af9527c2122630d909601fb8b330
RaulPabianCruz <ito.pabian@gmail.com>
2024-06-25 18:47:40 -0700

    Update Card component test files
    Completely overhauls card component test file so that it properly unit
    tests the component. useOutletState is mocked and the component is
    rendered on its own, outside of the RouterProvider context. Previously
    the test was more akin to an integration test than a unit test.

--- src/components/Card/Card.test.jsx
+++ src/components/Card/Card.test.jsx
@@ -1,31 +1,29 @@
 import { screen, render } from '@testing-library/react';
-import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import userEvent from '@testing-library/user-event';
+import { useOutletContext } from 'react-router-dom';
 import { vi } from 'vitest';
-import routes from '../../routes';
+import Card from './Card.jsx';
 
-const { mockItems } = vi.hoisted(() => ({
-  mockItems: [
-    {
-      id: 1,
-      title: 'Product Title',
-      price: 1,
-      category: 'Product',
-      description: 'Product Description.',
-      image: 'imageSrcLink',
-    },
-  ],
-}));
+vi.mock('react-router-dom', async () => {
+  const ReactRouterDom = await vi.importActual('react-router-dom');
 
-vi.mock('../Shop/hooks/useShopItems', () => ({
-  default: () => ({ shopItems: mockItems, error: null, loading: false }),
-}));
+  return { ...ReactRouterDom, useOutletContext: vi.fn() };
+});
+
+const mockItem = {
+  id: 1,
+  title: 'Product Title',
+  price: 1,
+  category: 'Product',
+  description: 'Product Description.',
+  image: 'imageSrcLink',
+};
 
 describe('Card Component', () => {
   describe('Card content', () => {
     it('renders card component content', () => {
-      const router = createMemoryRouter(routes, { initialEntries: ['/shop'] });
-      render(<RouterProvider router={router} />);
+      vi.mocked(useOutletContext).mockReturnValue([], null);
+      render(<Card product={mockItem} />);
 
       const img = screen.getByTestId('product-img');
       const title = screen.getByRole('heading', { name: 'Product Title' });
@@ -39,44 +37,85 @@ describe('Card Component', () => {
     });
 
     it("renders number input and 'Add to Cart' button", () => {
-      const router = createMemoryRouter(routes, { initialEntries: ['/shop'] });
-      render(<RouterProvider router={router} />);
+      vi.mocked(useOutletContext).mockReturnValue([], null);
+      render(<Card product={mockItem} />);
 
       const quantity = screen.getByRole('spinbutton', { name: 'Quantity:' });
       const addButton = screen.getByRole('button', { name: 'Add to Cart' });
 
       expect(quantity).toBeInTheDocument();
+      expect(quantity.value).toBe('1');
       expect(addButton).toBeInTheDocument();
-      screen.debug();
     });
   });
 
   describe('Card functionality', () => {
-    it('updates item count display when an item is added', async () => {
+    it("adds item to list of Cart items when 'Add to Cart' is clicked", async () => {
       const user = userEvent.setup();
-      const router = createMemoryRouter(routes, { initialEntries: ['/shop'] });
-      render(<RouterProvider router={router} />);
+      const setCartItems = vi.fn();
+      vi.mocked(useOutletContext).mockReturnValue([[], setCartItems]);
+      render(<Card product={mockItem} />);
 
       const addButton = screen.getByRole('button', { name: 'Add to Cart' });
-      const itemCount = screen.getByTestId('num-of-items');
       await user.click(addButton);
 
-      expect(itemCount.textContent).toBe('1');
+      expect(setCartItems).toHaveBeenCalledWith([
+        { product: mockItem, quantity: 1 },
+      ]);
     });
 
-    it('updates item count display based on quantity of item added', async () => {
+    it("adds item to list of Cart items when 'Add to Cart' is clicked, the quantity of the item is based on the quantity input", async () => {
       const user = userEvent.setup();
-      const router = createMemoryRouter(routes, { initialEntries: ['/shop'] });
-      render(<RouterProvider router={router} />);
+      const setCartItems = vi.fn();
+      vi.mocked(useOutletContext).mockReturnValue([[], setCartItems]);
+      render(<Card product={mockItem} />);
+
+      const itemQty = screen.getByRole('spinbutton', { name: 'Quantity:' });
+      const addButton = screen.getByRole('button', { name: 'Add to Cart' });
+      await user.click(itemQty);
+      await user.keyboard('[Backspace][Digit3]');
+      await user.click(addButton);
+
+      expect(setCartItems).toHaveBeenCalledWith([
+        { product: mockItem, quantity: 3 },
+      ]);
+    });
+
+    it("updates existing Cart item quantity correctly when 'Add to Cart' is clicked", async () => {
+      const user = userEvent.setup();
+      const setCartItems = vi.fn();
+      vi.mocked(useOutletContext).mockReturnValue([
+        [{ product: mockItem, quantity: 1 }],
+        setCartItems,
+      ]);
+      render(<Card product={mockItem} />);
+
+      const addButton = screen.getByRole('button', { name: 'Add to Cart' });
+      await user.click(addButton);
+
+      expect(setCartItems).toHaveBeenCalledWith([
+        { product: mockItem, quantity: 2 },
+      ]);
+    });
+
+    it("updates existing Cart item quantity correctly when 'Add to Cart' is clicked, updated quantity is based on the quantity input", async () => {
+      const user = userEvent.setup();
+      const setCartItems = vi.fn();
+      vi.mocked(useOutletContext).mockReturnValue([
+        [{ product: mockItem, quantity: 1 }],
+        setCartItems,
+      ]);
+      render(<Card product={mockItem} />);
 
       const productQtty = screen.getByRole('spinbutton', { name: 'Quantity:' });
       const addButton = screen.getByRole('button', { name: 'Add to Cart' });
-      const itemCount = screen.getByTestId('num-of-items');
       await user.click(productQtty);
       await user.keyboard('[Backspace][Digit5]');
       await user.click(addButton);
 
-      expect(itemCount.textContent).toBe('5');
+      expect(setCartItems).toHaveBeenCalledWith([
+        { product: mockItem, quantity: 6 },
+      ]);
     });
   });
 });
